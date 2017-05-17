b542e6b5b0cabf87b1a688fab3495163c5f32b83
Ricardo <universius@gmail.com>
2017-05-17 03:43:20 -0300

    checkpoint models databases

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 require './config/database'
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
+#Dir["./app/models/*.rb"].each do |file|
+#  require file
+#end
+
 
 class App < Sinatra::Base
   get '/sinatra' do
