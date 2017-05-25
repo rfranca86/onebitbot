e54f77ab7d944b0a45b4667bbd089704ee844b8e
rfranca86 <universius@gmail.com>
2017-05-25 13:32:46 -0300

    testando hashtags

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -3,15 +3,13 @@ module FaqModule
     def initialize(params)
       # TODO: identify origin and set company
       @company = Company.last
-      @question = params["question-original"]
-      @answer = params["answer-original"]
-      @hashtags = params["hashtags-original"]
+      @question = params["question.original"]
+      @answer = params["answer.original"]
+      @hashtags = params["hashtags.original"]
     end
 
     def call
-      if @hashtags == nil || @hashtags == ''
-        return "Hashtag Obrigatória"
-      end
+      return 'Hashtag Obrigatória' if @hashtags == nil || @hashtags == ''
       begin
         Faq.transaction do
           faq = Faq.create(question: @question, answer: @answer, company: @company)
@@ -19,6 +17,16 @@ module FaqModule
             faq.hashtags << Hashtag.create(name: hashtag)
           end
         end
+      # if @hashtags == nil || @hashtags == ''
+      #   return "Hashtag Obrigatória"
+      # end
+      # begin
+      #   Faq.transaction do
+      #     faq = Faq.create(question: @question, answer: @answer, company: @company)
+      #     @hashtags.split(/[\s,]+/).each do |hashtag|
+      #       faq.hashtags << Hashtag.create(name: hashtag)
+      #     end
+      #   end
         "Criado com sucesso"
       rescue
         "Problemas na criação"
