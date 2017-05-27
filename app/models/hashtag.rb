843dfcda8bf2263d152e14f677cd9e3aff0352a7
rfranca86 <universius@gmail.com>
2017-05-26 23:28:32 -0300

    corrigindo model hashtag

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -3,4 +3,6 @@ class Hashtag < ActiveRecord::Base
   belongs_to :company
   has_many :faq_hashtags
   has_many :faqs, through: :faq_hashtags
+  has_many :add_link_hashtags
+  has_many :add_links, through: :add_link_hashtags
 end
